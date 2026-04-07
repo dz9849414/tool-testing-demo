c9bdbc12522991f038dc55856fa1274eca0a09da
sunweizhou <s13809890602@163.com>
2026-04-07 11:42:13 +0800

    1。配置文件

---
+++ src/main/java/com/example/tooltestingdemo/ToolTestingDemoApplication.java
@@ -0,0 +1,11 @@
+package com.example.tooltestingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolTestingDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ToolTestingDemoApplication.class, args);
+    }
+}
