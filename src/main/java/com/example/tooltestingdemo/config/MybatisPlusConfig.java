aa7cbb0dbc9cea51011c170eead03b3c8f207004
dev_huangzhenyu <956648603@qq.com>
2026-04-21 11:01:15 +0800

    逻辑删除补丁&新maven依赖

--- src/main/java/com/example/tooltestingdemo/config/MybatisPlusConfig.java
+++ src/main/java/com/example/tooltestingdemo/config/MybatisPlusConfig.java
@@ -1,10 +1,15 @@
 package com.example.tooltestingdemo.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.config.GlobalConfig;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * MyBatis Plus配置类
@@ -22,6 +27,33 @@ public class MybatisPlusConfig {
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
         return interceptor;
     }
-    
+
+    /**
+     * 全局配置，包含逻辑删除配置
+     */
+    @Bean
+    public GlobalConfig globalConfig() {
+        GlobalConfig globalConfig = new GlobalConfig();
+        GlobalConfig.DbConfig dbConfig = new GlobalConfig.DbConfig();
+
+        // 配置逻辑删除
+        dbConfig.setLogicDeleteField("isDeleted"); // 逻辑删除字段名
+        dbConfig.setLogicDeleteValue("1");         // 已删除的值
+        dbConfig.setLogicNotDeleteValue("0");      // 未删除的值
+
+        globalConfig.setDbConfig(dbConfig);
+        return globalConfig;
+    }
+
+    /**
+     * 配置SqlSessionFactory，注入全局配置
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource, GlobalConfig globalConfig) throws Exception {
+        MybatisSqlSessionFactoryBean sessionFactory = new MybatisSqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setGlobalConfig(globalConfig);
+        return sessionFactory.getObject();
+    }
 
 }
