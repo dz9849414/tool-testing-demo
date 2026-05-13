9b4a2180866af94399e378898fdc0a01351d3555
sunweizhou <s13809890602@163.com>
2026-05-13 15:43:34 +0800

    1.setting文件

--- db-migration/2026-04-07/system_management_tables.sql
+++ db-migration/2026-04-07/system_management_tables.sql
@@ -162,10 +162,6 @@ CREATE TABLE `pdm_tool_sys_operation_log` (
 ALTER TABLE `tool_testing`.`pdm_tool_sys_operation_log`
 MODIFY COLUMN `ip_address` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT 'IP地址' AFTER `request_params`;
 
-ALTER TABLE `tool_testing`.`pdm_tool_sys_operation_log`
-    ADD COLUMN `method_json` varchar(1024) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '方法调用链JSON' AFTER `method`;
-
-
 -- 系统配置表
 DROP TABLE IF EXISTS `pdm_tool_sys_config`;
 CREATE TABLE `pdm_tool_sys_config` (
